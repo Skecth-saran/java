c7424bd1c11ef48e88a59216484e5e23a280ed1a
Saran <saransaran3489@gmail.com>
2023-06-07 22:50:01 +0530

    Create java6.java

---
+++ java6.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.io.*;
+
+public class PriorityQueueDemo {
+
+    public static void main(String args[]) {
+        PriorityQueue<String> pq = new PriorityQueue<>();
+
+        pq.add("I");
+        pq.add("Am");
+        pq.add("Saran");
+
+        System.out.println(pq);
+    }
+}
